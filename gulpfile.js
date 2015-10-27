808ce63a3b5a248fa329e5a21480d76f8b2797d8
Kyle Knott <kyle.knott@deutschinc.com>
2015-10-27 11:46:49 -0700

    upgrade react and add server code

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp       = require('gulp'),
     webpack    = require('gulp-webpack'),
     sass       = require('gulp-sass'),
     rename     = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    nodemon    = require('gulp-nodemon');
 
 gulp.task('webpack', function() {
   return gulp.src('./src/App.jsx')
@@ -18,3 +19,23 @@ gulp.task('sass', function() {
     .pipe(rename('style.css'))
     .pipe(gulp.dest('./public'));
 });
+
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.js' && './src/**/*.jsx', ['webpack']);
+  gulp.watch('./src/sass/*', ['sass']);
+});
+
+gulp.task('nodemon', function() {
+  nodemon({
+    script: './server.js',
+    ext: 'js jsx scss html'
+  })
+    .on('start', ['watch'])
+    .on('change', ['watch'])
+    .on('restart', function() {
+      console.log('Restarted');
+    });
+});
+
+gulp.task('default', ['nodemon'])
