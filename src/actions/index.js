e7cbeac353594fd09e0234cc50e376ce349fd30a
itsbigk <kyle.knott@jukinmedia.com>
2016-07-28 17:11:40 -0700

    remove async functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -30,9 +30,3 @@ export const incrementIfOdd = () => (dispatch, getState) => {
 
   dispatch(increment())
 }
-
-export const incrementAsync = (delay = 1000) => dispatch => {
-  setTimeout(() => {
-    dispatch(increment())
-  }, delay)
-}
