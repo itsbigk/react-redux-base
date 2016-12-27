7b90fd2ffc8150358241211fadb90b814d8ed613
itsbigk <kyle.knott@jukinmedia.com>
2016-12-27 13:01:11 -0800

    modify build files for prod

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { routes, configureStore } from '../shared'
+import { routes } from '../shared'
+import { configureStore } from '../shared/store'
 
 const initialState = window.__INITIAL_STATE__,
       store = configureStore(initialState),
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -53,7 +53,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: [/node_modules/, /\.spec\.jsx?$/],
-        include: path.join(__dirname, 'src'),
+        include: [path.join(__dirname, 'src'), path.join(__dirname, 'shared')],
         loader: 'babel'
       },
       {
